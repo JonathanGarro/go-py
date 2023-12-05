e9cd531eab9d3130c77b0ef74b2a52c65e6d68f2
Jonathan Garro <jonathan.garro@gmail.com>
2023-12-05 15:08:45 -0500

    update functions and add seaborn map

--- ifrc_go_py/module.py
+++ ifrc_go_py/module.py
@@ -1,8 +1,12 @@
 import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import pandas as pd
+from collections import Counter
 
 class SurgeAlert:
     def __init__(self, alert_id, message, molnix_id, created_at, opens, closes, start, end, region, modality, sector, scope, language, rotation, event_name, event_id, country_code):
-        
         self.alert_id = alert_id
         self.message = message
         self.molnix_id = molnix_id
@@ -23,61 +27,127 @@ class SurgeAlert:
         
     def __repr__(self):
         return f"SurgeAlert(alert_id={self.alert_id}, message={self.message})"
-        
-def get_latest_surge_alerts():
+
+class Appeal:
+    def __init__(self, aid, name, atype, atype_display, status, status_display, code, sector, num_beneficiaries, amount_requested, amount_funded, start_date, end_date, created_at, event, dtype_name, country_iso3, country_society_name):
+        self.aid = aid
+        self.name = name
+        self.atype = atype
+        self.atype_display = atype_display
+        self.status = status
+        self.status_display = status_display
+        self.code = code
+        self.sector = sector
+        self.num_beneficiaries = num_beneficiaries
+        self.amount_requested = amount_requested
+        self.amount_funded = amount_funded
+        self.start_date = start_date
+        self.end_date = end_date
+        self.created_at = created_at
+        self.event = event
+        self.dtype_name = dtype_name
+        self.country_iso3 = country_iso3
+        self.country_society_name = country_society_name
+
+def get_latest_appeals(atype=None):
     """
-    Returns the 50 latest surge alerts.
+    Returns the 50 latest appeals, with option to filter by appeal type.
     
-    >>> get_latest_surge_alerts()
-    [SurgeAlert(alert_id=18541, message=Humanitarian Diplomacy Coordinator, Middle East Crisis, MENA), SurgeAlert(alert_id=18540, message=Finance Officer, Hurricane Otis, Mexico.)...]
+    Args:
+        atype (int or None, optional): The appeal type to filter the appeals. 
+            0 = DREF
+            1 = Emergency Appeal
+    
+            If provided, filters the appeals based on the specified type. 
+            Defaults to None, retrieving all appeals if no type is specified.
+    
+    >>> get_latest_appeals()
+    [Appeal(alert_id=18541, message=Humanitarian Diplomacy Coordinator, Middle East Crisis, MENA), SurgeAlert(alert_id=18540, message=Finance Officer, Hurricane Otis, Mexico.)...]
     """
     
-    api_call = 'https://goadmin.ifrc.org/api/v2/surge_alert/'
+    if atype is not None:
+        api_call = f'https://goadmin.ifrc.org/api/v2/appeal/?atype={atype}'
+    else:
+        api_call = 'https://goadmin.ifrc.org/api/v2/appeal/'
+    
     r = requests.get(api_call).json()
-    
-    surge_alerts = []
+    print(r)
+    appeals = []
     
     for result in r['results']:
-        alert_id = result['id']
-        message = result['message']
-        molnix_id = result['molnix_id']
-        created_at = result['created_at']
-        opens = result['opens']
-        closes = result['closes']
-        start = result['start']
-        end = result['end']
+        aid = result.get('aid')
+        name = result.get('name')
+        atype = result.get('atype')
+        atype_display = result.get('atype_display')
+        status = result.get('status')
+        status_display = result.get('status_display')
+        code = result.get('code')
+        sector = result.get('sector')
+        num_beneficiaries = result.get('num_beneficiaries')
+        amount_requested = result.get('amount_requested')
+        amount_funded = result.get('amount_funded')
+        start_date = result.get('start_date')
+        end_date = result.get('end_date')
+        created_at = result.get('created_at')
+        event = result.get('event')
+        dtype_name = result.get('dtype_name')
+        country_info = result.get('country', {})
+        country_iso3 = country_info.get('iso3')
+        country_society_name = country_info.get('society_name')
         
-        region, modality, sector, scope, language, rotation, event_name, event_id, country_code = '', '', '', '', '', '', '', '', ''
+        # create appeal object and append to appeals list
+        appeal = Appeal(aid, name, atype, atype_display, status, status_display, code, sector, num_beneficiaries, amount_requested, amount_funded, start_date, end_date, created_at, event, dtype_name, country_iso3, country_society_name)
+        appeals.append(appeal)
+
         
-        for tag in result['molnix_tags']:
-            groups = tag['groups']
-            if 'LANGUAGE' in groups:
-                language = tag['description']
-            if 'rotation' in groups:
-                rotation = tag['description']
-            if 'ALERT TYPE' in groups:
-                scope = tag['description']
-            if 'Modality' in groups:
-                modality = tag['description']
-            if 'REGION' in groups:
-                region = tag['description']
-            if 'OPERATIONS' in groups:
-                event_name = tag['description']
-                event_id = tag['name']
-            if 'ROLES' in groups:
-                try:
-                    next_index = groups.index('ROLES') + 1
-                    sector = groups[next_index]
-                except IndexError:
-                    sector = 'Missing Sector'
-                    
-        try:
-            country_code = result['event']['countries'][0]['iso3']
-        except:
-            country_code = 'Missing Country'
-            
-        # create SurgeAlert object and append to surge_alerts list
-        surge_alert = SurgeAlert(alert_id, message, molnix_id, created_at, opens, closes, start, end, region, modality, sector, scope, language, rotation, event_name, event_id, country_code)
-        surge_alerts.append(surge_alert)
-        
-    return surge_alerts
+    return appeals
+
+def plot_countries_by_iso3(appeals):
+    """
+    Pass in a list of Appeal objects and generate a map at admin0 countries.
+    
+    Args:
+        None
+    """
+    
+    iso3_countries = [appeal.country_iso3 for appeal in appeals if appeal.country_iso3]
+    
+    if not iso3_countries:
+        print("No ISO3 country codes found in the provided data.")
+        return
+    
+    # remove Antarctica from the list of iso3 codes
+    iso3_countries = [iso for iso in iso3_countries if iso != 'ATA']
+    
+    iso3_counter = Counter(iso3_countries)
+    
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    
+    world_iso3 = world.merge(
+        pd.DataFrame.from_dict(iso3_counter, orient='index', columns=['Appeal_Count']).reset_index(),
+        how='left',
+        left_on='iso_a3',
+        right_on='index'
+    )
+    
+    world_iso3['Appeal_Count'] = world_iso3['Appeal_Count'].fillna(0)
+    
+    # remove antarctica from the map data
+    world_iso3 = world_iso3[world_iso3['continent'] != 'Antarctica']
+    
+    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+    
+    # plot the map
+    ax.axis('off')
+    ax.set_title('Choropleth Map of Appeals per Country')
+    
+    world_iso3.plot(column='Appeal_Count', ax=ax, legend=True, cmap='viridis', 
+                    legend_kwds={'label': "Number of Appeals", 'orientation': "horizontal",
+                                 'ticks': range(0, int(world_iso3['Appeal_Count'].max()) + 1)},
+                    edgecolor='none')  # Remove boundary lines
+    
+    # filter out antarctica
+    world_iso3[world_iso3['Appeal_Count'] == 0].plot(ax=ax, color='lightgrey')
+    
+    plt.tight_layout()  # Adjust layout for better visualization
+    plt.show()
